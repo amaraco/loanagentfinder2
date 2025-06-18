3991fc508f314bde8aadda4aa02d787be303b384
amaraco <morgan@amaracoagency.com>
2025-06-17 18:53:51 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to LoanAgentFinder</h1>
+      <p>Start your search by visiting: <code>/search?zip=38119</code></p>
+    </div>
+  );
+}
